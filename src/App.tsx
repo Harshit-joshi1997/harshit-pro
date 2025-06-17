a3abc20b794c130d534ec2c21312acaaf92d75c5
Harshit-joshi1997 <joshiharshit291@gmail.com>
2025-06-17 20:36:22 +0530

    login page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Page from './App/login/page'
 
 function App() {
  
 
   return (
-    <>
+     <Page />
+
       
-    </>
+    
   )
 }
 
---
+++ src/App/login/Page.tsx
@@ -0,0 +1,25 @@
+import { GalleryVerticalEnd } from "lucide-react"
+
+import { LoginForm } from "@/components/login-form"
+
+
+export default function LoginPage() {
+  return (
+        <div
+      className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 bg-cover bg-center"
+      style={{ backgroundImage: "url('/src/assets/cardpic.jpg')" ,borderRadius:"10px"}} // Change '/bg.jpg' to your image path
+    > 
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className=" size-4" />
+            
+          </div>
+          Royal Inc.
+        </a>
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
+
