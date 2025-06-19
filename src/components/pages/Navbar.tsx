f2a59578b2847827ee0f0e833e57d29838454a29
Harshit-joshi1997 <joshiharshit291@gmail.com>
2025-06-19 21:33:27 +0530

    dropdown menu is fixed

--- src/components/pages/Navbar.tsx
+++ src/components/pages/Navbar.tsx
@@ -1,6 +1,7 @@
-
+import React, { useState } from 'react';
 
 const Navbar = () => {
+   const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="bg-gray-800" style={{  width:"1519px",marginLeft:"-152px",marginTop:"-342px",padding:"8px"}}>
   <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -54,19 +55,20 @@ const Navbar = () => {
         {/* Profile dropdown */}
         <div className="relative ml-3">
           <div>
-            <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+            <button onClick ={()=> setIsOpen ((prev) => !prev)}type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden" id="user-menu-button" aria-expanded={isOpen} aria-haspopup="true">
               <span className="absolute -inset-1.5"></span>
               <span className="sr-only">Open user menu</span>
               <img className="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
             </button>
           </div>
-
+         {isOpen && (
           <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-hidden" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
             {/* Active: "bg-gray-100 outline-hidden", Not Active: "" */}
             <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-0">Your Profile</a>
             <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-1">Settings</a>
             <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-2">Sign out</a>
           </div>
+         )}
         </div>
       </div>
     </div>
